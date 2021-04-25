537370ebf778a8afac3597b870c6ca567bb51865
Mark Page <mark.page@burdenandburden.co.uk>
2021-04-25 16:03:33 +0000

    Adds private method supporter_add() and placeholder for method complete(), improves try/catch structure for callback

--- PayApi.php
+++ PayApi.php
@@ -43,45 +43,29 @@ class PayApi {
 
     public function callback ( ) {
         try {
+            $error = null;
+            $step = null;
             // Do Paypal stuff
+            $this->complete ($txn_ref);
+            $this->supporter_add ($txn_ref);
+            // Say OK back to Paypal
+            // Send confirmation email
+            if (PAYPAL_CMPLN_EM) {
+                $step = 'Confirmation email';
+                $this->campaign_monitor ($supporter_nr,$tickets,$first_draw_close,$draws);
+            }
+            // Send confirmation SMS
+            if (PAYPAL_CMPLN_PH) {
+                $step = 'Confirmation SMS';
+                $sms        = new \SMS ();
+                $details    = sms_message ();
+                $sms->send ($_POST['mobile'],$details['message'],$details['from']);
+            }
+            return true;
         }
         catch (\Exception $e) {
-            // Say FOOEY back to Paypal
-        }
-        $error = null;
-        $step = null;
-        try {
-            // Update paypal_payment - `Paid`=NOW(),`Created`=CURDATE()
-            // Insert a supporter, a player and a contact
-            //     canvas code is PAYPAL_CODE
-            //     canvas_ref is new insert ID
-            //     RefNo == canvas_ref + 100000
-            //     Provider = PAYPAL_CODE
-            //     ClientRef = PAYPAL_CODE . Refno
-            // Em olrait?
-            // Assign tickets by updating blotto_ticket
-            try {
-                // Say OK back to Paypal
-                // Send confirmation email
-                if (PAYPAL_CMPLN_EM) {
-                    $step = 'Confirmation email';
-                    $this->campaign_monitor ($supporter_nr,$tickets,$first_draw_close,$draws);
-                }
-                // Send confirmation SMS
-                if (PAYPAL_CMPLN_PH) {
-                    $step = 'Confirmation SMS';
-                    $sms        = new \SMS ();
-                    $details    = sms_message ();
-                    $sms->send ($_POST['mobile'],$details['message'],$details['from']);
-                }
-                return true;
-            }
-            catch (\Exception $e) {
-                $error = 'TXN_REF '.$txn_ref.' '.$step.': '.$e->getMessage();
-            }
-        }
-        catch (\mysqli_sql_exception $e) {
-            $error = 'TXN_REF '.$txn_ref.' SQL exception: '.$e->getMessage();
+            $error = "Error for txn=$txn_ref: {$e->getMessage()}";
+            // Now saySay FOOEY back to Paypal
         }
         error_log ($error);
         mail (
@@ -89,6 +73,7 @@ class PayApi {
             'Paypal sign-up callback error',
             $error
         );
+        return false;
     }
 
     private function campaign_monitor ($ref,$tickets,$first_draw_close,$draws) {
@@ -117,6 +102,10 @@ class PayApi {
             'unchanged'
         );
         // error_log ('Campaign Monitor result: '.print_r($result,true));
+    }
+
+    private function complete ($txn_ref) {
+        // Update paypal_payment - `Paid`=NOW() where txn_ref=...
     }
 
     private function error_log ($code,$message) {
@@ -295,6 +284,80 @@ class PayApi {
             'from' => PAYPAL_SMS_FROM,
             'message' => PAYPAL_SMS_MESSAGE
         ];
+    }
+
+    private function supporter_add ($txn_ref) {
+        try {
+            $supporter = $this->connection->query (
+              "SELECT * FROM `stripe_payment` WHERE `txn_ref`='$txn_ref' LIMIT 0,1"
+            );
+            $supporter = $supporter->fetch_assoc ();
+            if (!$supporter) {
+                throw new \Exception ("Transaction reference '$txn_ref' was not identified");
+            }
+        }
+        catch (\mysqli_sql_exception $e) {
+            $this->error_log (122,'SQL select failed: '.$e->getMessage());
+            throw new \Exception ('SQL error');
+            return false;
+        }
+        $ccc        = STRIPE_CODE;
+        $provider   = STRIPE_CODE;
+        $refno      = STRIPE_REFNO_OFFSET + $supporter['id'];
+        $cref       = STRIPE_CODE.'_'.$refno;
+        // Insert a supporter, a player and a contact
+        try {
+            $this->connection->query (
+              "
+                INSERT INTO `blotto_supporter` SET
+                  `created`=DATE('{$['created']}')
+                 ,`signed`=DATE('{$['created']}')
+                 ,`approved`=DATE('{$['created']}')
+                 ,`canvas_code`='$ccc'
+                 ,`canvas_agent_ref`='$ccc'
+                 ,`canvas_ref`='{$['id']}'
+                 ,`client_ref`='$cref'
+              "
+            );
+            $sid = $this->connection->lastInsertId ();
+            $this->connection->query (
+              "
+                INSERT INTO `blotto_player` SET
+                 ,`started`=DATE('{$['created']}')
+                 ,`supporter_id`=$sid
+                 ,`client_ref`='$cref'
+                 ,`chances`={$['quantity']}
+              "
+            );
+            $this->connection->query (
+              "
+                INSERT INTO `blotto_contact` SET
+                  `supporter_id`=$sid
+                 ,`title`='{$['title']}'
+                 ,`name_first`='{$['first_name']}'
+                 ,`name_last`='{$['last_name']}'
+                 ,`email`='{$['email']}'
+                 ,`mobile`='{$['mobile']}'
+                 ,`telephone`='{$['telephone']}'
+                 ,`address_1`='{$['address_1']}'
+                 ,`address_2`='{$['address_2']}'
+                 ,`address_3`='{$['address_3']}'
+                 ,`town`='{$['town']}'
+                 ,`county`='{$['county']}'
+                 ,`postcode`='{$['postcode']}'
+                 ,`dob`='{$['dob']}'
+                 ,`p0`='{$['pref_1']}'
+                 ,`p1`='{$['pref_2']}'
+                 ,`p2`='{$['pref_3']}'
+                 ,`p3`='{$['pref_4']}'
+              "
+            );
+        }
+        catch (\mysqli_sql_exception $e) {
+            $this->error_log (121,'SQL insert failed: '.$e->getMessage());
+            throw new \Exception ('SQL error');
+            return false;
+        }
     }
 
     private function verify_email ($email) {
