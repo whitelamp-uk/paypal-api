2165fd765da5bd4466aea6107492cdd2c3b15917
Mark Page <mark.page@burdenandburden.co.uk>
2021-04-27 17:15:33 +0000

    Removes lots of functionality which is now general purpose in blotto2

--- PayApi.php
+++ PayApi.php
@@ -132,68 +132,6 @@ class PayApi {
         return $output;
     }
 
-    private function form_vars ( ) {
-        $vars = array (
-            'title' => '',
-            'first_name' => '',
-            'last_name' => '',
-            'dob' => '',
-            'postcode' => '',
-            'address_1' => '',
-            'address_2' => '',
-            'address_3' => '',
-            'town' => '',
-            'county' => '',
-            'quantity' => '',
-            'draws' => '',
-            'pref_1' => '',
-            'pref_2' => '',
-            'pref_3' => '',
-            'pref_4' => '',
-            'telephone' => '',
-            'mobile' => '',
-            'email' => '',
-            'gdpr' => '',
-            'terms' => '',
-            'age' => '',
-            'signed' => ''
-        );
-        foreach ($_POST as $k=>$v) {
-            $vars[$k] = $v;
-        }
-        return $vars;
-    }
-
-    function get_address ( ) {
-        if (ctype_digit($_POST['address_1'][0])) {
-            $firstline          = explode(' ', $_POST['address_1'], 2);
-            $house_number       = $firstline[0];
-            $address_1          = $firstline[1];
-            $address_2          = $_POST['address_2'];
-            if ($_POST['address_3']) {
-                $address_2     .= ', '.$_POST['address_3'];
-            }
-            $house_name         = '';
-        }
-        else {
-            $house_name         = $_POST['address_1'];
-            $address_1          = $_POST['address_2'];
-            $address_2          = $_POST['address_3'];
-            $house_number       = '';
-        }
-        $address_obj            = new \stdClass ();
-        $address_obj->city          = $_POST['town'];
-        $address_obj->county        = $_POST['county'];
-        $address_obj->country       = 'GB';
-        $address_obj->postcode      = $_POST['postcode'];
-        $address_obj->address_1     = $address_1;
-        $address_obj->address_2     = $address_2;
-        $address_obj->house_name    = $house_name;
-        $address_obj->house_number  = $house_number;
-        $address = json_encode ($address_obj);
-        return $address;
-    }
-
     public function import ($from) {
         $from               = new \DateTime ($from);
         $this->from         = $from->format ('Y-m-d');
@@ -215,12 +153,6 @@ class PayApi {
             throw new \Exception ('SQL error');
             return false;
         }
-    }
-
-    public function output_signup_form ( ) {
-        $v = $this->form_vars ();
-        $titles = explode (',',BLOTTO_TITLES_WEB);
-        require __DIR__.'/form.php';
     }
 
     private function output_mandates ( ) {
@@ -259,24 +191,8 @@ class PayApi {
     }
 
     public function start ( ) {
-         verify_general ();
-        if ($_POST['telephone'] && !verify_phone($_POST['telephone'])) {
-            $error = 'Telephone number (landline) is not valid';
-        }
-        elseif ($_POST['mobile'] && !verify_phone($_POST['mobile'],'m')) {
-            $error = 'Telephone number (mobile) is not valid';
-        }
-        elseif ($_POST['email'] && !verify_email($_POST['email'])) {
-            $error = 'Email address is not valid';
-        }
-        else {
-            // Insert into paypal_payment leaving especially `Paid` and `Created` as null
-            // $this->txn_ref = something unique to go in button
-        }
-        if ($error) {
-            throw new \Exception ($error);
-            return false;
-        }
+        // Insert into paypal_payment leaving especially `Paid` and `Created` as null
+        // $this->txn_ref = something unique to go in button
     }
 
     private function sms_message ( ) {
@@ -360,138 +276,7 @@ class PayApi {
         }
     }
 
-    private function verify_email ($email) {
-        $params = array(
-            "username" => PAYPAL_D8_USERNAME,
-            "password" => PAYPAL_D8_PASSWORD,
-            "email" => $email,
-            "level" => PAYPAL_EMAIL_VERIFY_LEVEL,
-        );
-        $client = new \SoapClient ("https://webservices.data-8.co.uk/EmailValidation.asmx?WSDL");
-        $result = $client->IsValid ($params);
-        if ($result->IsValidResult->Status->Success == false) {
-            throw new \Exception ("Error trying to validate email: ".$result->Status->ErrorMessage);
-            return false;
-        }
-        if ($result->IsValidResult->Result=='Invalid') {
-            define ( 'PAYPAL_GO', 'contact');
-            throw new \Exception ("$email is an invalid address");
-            return false;
-        }
-        return true;
-    }
-
-    public function verify_general ( ) {
-        foreach ($_POST as $key => $value) {
-            $_POST[$key] = trim($value);
-        }
-        if (!$_POST['title']) {
-            define ( 'PAYPAL_GO', 'about');
-            throw new \Exception ('Title is required');
-            return false;
-        }
-        if (!$_POST['first_name']) {
-            define ( 'PAYPAL_GO', 'about');
-            throw new \Exception ('First name is required');
-            return false;
-        }
-        if (!$_POST['last_name']) {
-            define ( 'PAYPAL_GO', 'about');
-            throw new \Exception ('Last name is required');
-            return false;
-        }
-        if (!$_POST['dob']) {
-            define ( 'PAYPAL_GO', 'about');
-            throw new \Exception ('Date of birth is required');
-            return false;
-        }
-        $dt             = new \DateTime ($_POST['dob']);
-        if (!$dt) {
-            define ( 'PAYPAL_GO', 'about');
-            throw new \Exception ('Date of birth is not valid');
-            return false;
-        }
-        $now        = new \DateTime ();
-        $years      = $dt->diff($now)->format ('%r%y');
-        if ($years<18) {
-            throw new \Exception ('You must be 18 or over to sign up');
-            return false;
-        }
-        if (!$_POST['postcode']) {
-            define ( 'PAYPAL_GO', 'address');
-            throw new \Exception ('Postcode is required');
-            return false;
-        }
-        if (!$_POST['address_1']) {
-            define ( 'PAYPAL_GO', 'address');
-            throw new \Exception ('First line of address is required');
-            return false;
-        }
-        if (!$_POST['town']) {
-            define ( 'PAYPAL_GO', 'address');
-            throw new \Exception ('Town/city is required');
-            return false;
-        }
-        if (!$_POST['sort_code']) {
-            define ( 'PAYPAL_GO', 'bank');
-            throw new \Exception ('Bank sort code is required');
-            return false;
-        }
-        if (!$_POST['account_number']) {
-            define ( 'PAYPAL_GO', 'bank');
-            throw new \Exception ('Bank account number is required');
-            return false;
-        }
-        if (!array_key_exists('gdpr',$_POST) || !$_POST['gdpr']) {
-            define ( 'PAYPAL_GO', 'gdpr');
-            throw new \Exception ('You must confirm that you have read the GDPR statement');
-            return false;
-        }
-        if (!array_key_exists('signed',$_POST) || !$_POST['signed']) {
-            define ( 'PAYPAL_GO', 'sign');
-            throw new \Exception ('You must confirm your direct debit instruction');
-            return false;
-        }
-        if (!array_key_exists('terms',$_POST) || !$_POST['terms']) {
-            define ( 'PAYPAL_GO', 'sign');
-            throw new \Exception ('You must agree to terms & conditions and the privacy policy');
-            return false;
-        }
-        if (!array_key_exists('age',$_POST) || !$_POST['age']) {
-            define ( 'PAYPAL_GO', 'sign');
-            throw new \Exception ('You must be aged 18 or over to signup');
-            return false;
-        }
-        return true;
-    }
-
-    function verify_phone ($number, $type='l') {
-        $params = array(
-            "username" => D8_USERNAME,
-            "password" => D8_PASSWORD,
-            "telephoneNumber" => $number,
-            "defaultCountry" => 'GB',
-        );
-        $params['options']['Option'][] =  array("Name" => "UseMobileValidation", "Value" => false);
-        $params['options']['Option'][] =  array("Name" => "UseLineValidation", "Value" => false);
-        $client = new \SoapClient ("https://webservices.data-8.co.uk/InternationalTelephoneValidation.asmx?WSDL");
-        $result = $client->IsValid($params);
-        if ($result->IsValidResult->Status->Success == false) {
-            throw new \Exception ("Error trying to validate phone number: ".$result->Status->ErrorMessage);
-            return false;
-        }
-        if ($result->IsValidResult->Result->ValidationResult=='Invalid') {
-            define ( 'PAYPAL_GO', 'contact');
-            throw new \Exception ("$number is not a valid phone number");
-            return false;
-        }
-        elseif ($type == 'm' && $result->IsValidResult->Result->NumberType!='Mobile') {
-            define ( 'PAYPAL_GO', 'contact');
-            throw new \Exception ("$number is not a valid mobile phone number");
-            return false;
-        }
-        return true;
-    }
-
 }
+
+require_once PAYPAL_CAMPAIGN_MONITOR;
 
