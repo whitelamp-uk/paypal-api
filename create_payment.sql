41fd66875856ea10dd79667aff87f6c62562faa1
Mark Page <mark.page@burdenandburden.co.uk>
2021-06-14 11:05:04 +0000

    Moves _readme table to payment SQL file

--- create_payment.sql
+++ create_payment.sql
@@ -1,4 +1,13 @@
 
+CREATE TABLE IF NOT EXISTS `_readme` (
+  `project` char(64),
+  `location` varchar(255) NOT NULL,
+  PRIMARY KEY (`project`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+INSERT IGNORE INTO `_readme` (`project`, `location`) VALUES
+('whitelamp-uk/paypal-api', 'https://github.com/whitelamp-uk/paypal-api.git');
+
+
 -- A persistent table in BLOTTO_MAKE_DB
 CREATE TABLE IF NOT EXISTS `paypal_payment` (
   `id` INT (11) NOT NULL AUTO_INCREMENT,
